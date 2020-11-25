f298576b07b33063237d07f3ea5d7e386d47a7e1
cvida <abascadiv@gmail.com>
2020-11-25 14:43:36 +0100

    jolesz

--- readJson.py
+++ readJson.py
@@ -1,4 +1,5 @@
 import os
+import random
 from os.path import isfile, join
 import json
 import ftfy
@@ -33,41 +34,187 @@ def processDateStat(dateStat, date):
     yearMonth = year + "-" + month
     yearMonthDay = year + "-" + month + "-" + day
 
-    # if year in dateStat:
-    #     recentYear = int(dateStat[year])
-    #     dateStat[year] = recentYear + 1
-    # else:
-    #     dateStat[year] = 1
     if yearMonth in dateStat:
         recentYearMonth = int(dateStat[yearMonth])
         dateStat[yearMonth] = recentYearMonth + 1
     else:
         dateStat[yearMonth] = 1
-    # if yearMonthDay in dateStat:
-    #     recentYearMonth = int(dateStat[yearMonthDay])
-    #     dateStat[yearMonthDay] = recentYearMonth + 1
-    # else:
-    #     dateStat[yearMonthDay] = 1
-
-    #print("\n")
-    #print(dateStat)
     return dateStat
 
 
+def getScript(dates, allMessageValues, p1MessageValues, p2MessageValues, person1, person2):
+    # print("eleje")
+    # print(allMessageValues)
+    # print(p1MessageValues)
+    # print(p2MessageValues)
+    # print(person1)
+    # print(person2)
+    #exit()
+    # ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+    # [
+    # 	randomScalingFactor(),
+    # 	randomScalingFactor(),
+    # 	randomScalingFactor(),
+    # 	randomScalingFactor(),
+    # 	randomScalingFactor(),
+    # 	randomScalingFactor(),
+    # 	randomScalingFactor()
+    # ]
+
+    script = """
+    <script>
+        window.randomScalingFactor = function() {
+		return Math.floor(Math.random() * 100); 
+	};
+		var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+		var config = {
+			type: 'line',
+			data: {
+				labels: """ + str(list(dates))+ """,
+				datasets: [{
+					label: 'All messages',
+					backgroundColor: '#C21990',
+					borderColor: '#C21990',
+					data: 
+					
+					""" + str(list(allMessageValues)) + """
+					,
+					fill: false,
+				},
+				 {
+					label: ' """  + str(person1) + """ ',
+					backgroundColor: '#19B8C2',
+					borderColor: '#19B8C2',
+					data: 
+					""" + str(list(p1MessageValues)) + """,
+					fill: false,
+				},
+				 {
+					label: ' """  + str(person2) + """ ',
+					fill: false,
+					backgroundColor: '#C29919',
+					borderColor: '#C29919',
+					data: 
+					""" + str(list(p2MessageValues)) + """,
+				}]
+			},
+			options: {
+				responsive: true,
+				title: {
+					display: true,
+					text: 'Chart.js Line Chart'
+				},
+				tooltips: {
+					mode: 'index',
+					intersect: false,
+				},
+				hover: {
+					mode: 'nearest',
+					intersect: true
+				},
+				scales: {
+					xAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Month'
+						}
+					}],
+					yAxes: [{
+						display: true,
+						scaleLabel: {
+							display: true,
+							labelString: 'Value'
+						}
+					}]
+				}
+			}
+		};
+
+		window.onload = function() {
+			var ctx = document.getElementById('canvas').getContext('2d');
+			window.myLine = new Chart(ctx, config);
+		};
+
+		
+
+		var colorNames = Object.keys(window.chartColors);
+		document.getElementById('addDataset').addEventListener('click', function() {
+			var colorName = colorNames[config.data.datasets.length % colorNames.length];
+			var newColor = window.chartColors[colorName];
+			var newDataset = {
+				label: 'Dataset ' + config.data.datasets.length,
+				backgroundColor: newColor,
+				borderColor: newColor,
+				data: [],
+				fill: false
+			};
+
+			for (var index = 0; index < config.data.labels.length; ++index) {
+				newDataset.data.push(randomScalingFactor());
+			}
+
+			config.data.datasets.push(newDataset);
+			window.myLine.update();
+		});
+
+		
+
+		
+		
+	</script>
+	</body>
+                </html>
+    """
+
+    scriptStart = """
+            <script>
+            new Chart(document.getElementById("line-chart"), {
+      type: 'line',
+      data: {
+        labels: 
+            """
+
+    dataSetStart = """
+                ,
+            datasets: ["""
+    dataStart =       """{ 
+                data: 
+                """
+    dataSetEnd = """
+                ,
+                label: "All messages",
+                borderColor: "#3eeeee",
+                fill: false
+              }"""
+    scriptEnd ="""]
+          },
+          options: {
+            title: {
+              display: true,
+              text: 'World population per region (in millions)'
+            }
+          }
+        });
+                </script>
+                </body>
+                </html>
+                """
+    content = script
+    return content
+
+
 def generateContent(keys, values, data1):
-    print(data1)
     print(len(data1["messages"]))
+    #print(data1["messages"])
     #exit()
     templateStart = """
     <!DOCTYPE html>
-    <head> 
+    <head lang="hu"> 
     <meta charset="UTF-8">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw==" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-    <style type="text/css">
-   
-</style>
-    <title>Template {{ title }}</title>
+       <title>Template {{ title }}</title>
     </head>
     <body>
     <div class="container"> 
@@ -75,41 +222,8 @@ def generateContent(keys, values, data1):
     </div>
     
     """
-    templateEnd1 = """
-        <script>
-        new Chart(document.getElementById("line-chart"), {
-  type: 'line',
-  data: {
-    labels: 
-        """
 
-    templateEnd2 = """
-            ,
-        datasets: [{ 
-            data: 
-            """
-    templateEnd3 = """
-            ,
-            label: "Africa",
-            borderColor: "#3e95cd",
-            fill: false
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'World population per region (in millions)'
-        }
-      }
-    });
-            </script>
-            </body>
-            </html>
-            """
     content = templateStart
-    print(data1["first"] )
-    print(data1["last"] )
     stringem = str(data1["first"])
     utso = str(data1["last"])
     name = data1["name"]
@@ -119,6 +233,10 @@ def generateContent(keys, values, data1):
     timeDiff = data1["diff"]
     redWords=data1["redWords"]
     words = data1["words"]
+    person1 = data1["person1"]
+    person2 = data1["person2"]
+    #person1Values = data1[person1]
+    #person2Values = data1[person2]
     listToStr = ', '.join([str(elem) for elem in redWords])
     if lastMessage:
         lastM = str(data1["last"])
@@ -167,14 +285,14 @@ def generateContent(keys, values, data1):
   </tbody>
 </table></div>
     <div class="col">Column</div>
-    <div class="col"><canvas id="line-chart" width="auto" height="auto"></canvas></div>"""
+    <div class="col"><canvas id="canvas" width="600" height="500"></canvas></div>"""
 
     for id, mess in myMessages.items():
-        content += '<div class="col"><p>'
+        content += '<div class="col blue">'
         print(mess)
         content += '<a href="#" class="text-light bg-dark"> ' + str(id) + " : </a>"
         content += mess
-        content += '</p><div>'
+        content += '<div>'
 
 
     #exit()
@@ -183,30 +301,28 @@ def generateContent(keys, values, data1):
   </div>
 </div>
 """
-    content += templateEnd1
-    content +=str(list(keys))
-    content += templateEnd2
-    content += str(list(values))
-    content += templateEnd3
-
-
+    print(values)
+    allMessages =  values["all"]
+    print(allMessages)
+    content += getScript(keys, allMessages, values[person1], values[person2], person1, person2)
     return content
 
 
 def processWord(words, toProcess):
     print(words)
     wordUnits = toProcess.split(" ")
+    wordUnits = map(lambda x: x.lower(), wordUnits)
     words.extend(wordUnits)
-
     print(wordUnits)
     ##print(words)
-    print("ANYÁD")
     return sorted(set(words))
 
 
 def processJson(file):
     print(file)
     dateStat = {}
+    person1Stat = {}
+    person2Stat = {}
     fileName = file
     print("fileName = "  + fileName)
     print(fileName)
@@ -221,21 +337,28 @@ def processJson(file):
         print(data)
         print(names)
 
+
         person1 = names[0]["name"]
+        person1 = encodeText(person1)
+
         person2 = names[1]["name"]
+        person2 = encodeText(person2)
+
         messages = data['messages']
         firstMess = messages[len(messages)-1]['timestamp_ms']
         lastMess = messages[0]['timestamp_ms']
-        htmlFilename = encodeText(person1) + "-" + encodeText(person2) + " " + getDate(firstMess).split()[0] + "---" + \
-                       getDate(lastMess).split()[0] + "_index.html"
+        mess_count = len(messages)
+        htmlFilename = str(mess_count) + " " + encodeText(person1) + "-" + encodeText(person2) + " " + getDate(firstMess).split()[0] + "---" + \
+                       getDate(lastMess).split()[0]  +".html"
         htmlFilename = htmlFilename.replace(" ", "_")
         print(htmlFilename)
         if (os.path.exists(htmlFilename)):
             return 0
         print(person1)
         print(person2)
+        print("végeee")
+        #exit()
          #print(messages)
-        mess_count = len(messages)
         print("DArab üzenet " + str(mess_count))
         print(messages[0])
         timeDiff = lastMess - firstMess
@@ -243,39 +366,60 @@ def processJson(file):
         counter = 0
         myMessages = {}
         words = []
-        for m in messages:
+        for message in messages:
             print("\n")
-            if "content" in m:
-                data = m['content']
-                #print(data)
-                # print(encodeText(data))
+            name = encodeText(message['sender_name'])
+            if "content" in message:
+                data = message['content']
+                print(encodeText(data))
+
                 # https://stackoverflow.com/questions/26614323/in-what-world-would-u00c3-u00a9-become-%C3%A9
-            if ((m['sender_name'] == person2) & ("content" in m)):
+            if ((name == person2) & ("content" in message)):
+                print(data)
+                #exit()
                 print(counter)
                 csabauzik = csabauzik + 1
-                date = getDate(m['timestamp_ms'])
-                print(encodeText(person2) + " " + date + " : ")
+                date = getDate(message['timestamp_ms'])
+                print(encodeText(person2) + " [" + date + "] : ")
                 dateStat = processDateStat(dateStat, date)
-                datam = m['content']
-                print(encodeText(datam))
+                person2Stat = processDateStat(person2Stat, date)
+                datam = message['content']
+                #print(encodeText(datam))
 
                 toProcess = encodeText(datam)
                 #
                 words = processWord(words, toProcess)
                 message = encodeText(person2 + " " + date + " : \n " + datam)
                 myMessages[counter] = message
-            if ((m['sender_name'] == person1) & ("content" in m)):
+
+            print(person1)
+            print(name)
+            print("faszom")
+           #exit()
+            if ((name == person1) & ("content" in message)):
+                print(person1)
+                print("BEAA")
+                print(data)
+                #exit()
+                print(data)
+                print(name)
+                print("szar p1")
+                #exit()
                 print(counter)
-                date = getDate(m['timestamp_ms'])
+                date = getDate(message['timestamp_ms'])
                 print(encodeText(person1 ) + " " + date + " : ")
                 dateStat = processDateStat(dateStat, date)
-                datam = m['content']
+                person1Stat = processDateStat(person1Stat, date)
+                datam = message['content']
                 #print(encodeText(datam))
                 toProcess = encodeText(datam)
                 #
                 words = processWord(words, toProcess)
                 message = encodeText(person1 + " " + date + " : \n " + datam)
                 myMessages[counter] = message
+                print("==========")
+                print(message)
+                print("__")
             counter += 1
 
 
@@ -293,18 +437,24 @@ def processJson(file):
         print("ODA")
         dateStatList = list(dateStat.keys())[::-1]
         dateStatKValues = list(dateStat.values())[::-1]
-        print(dateStatList)
-        print(dateStatKValues)
-        print("___DEBUG___")
-        print(person1)
-        print(person2)
-        print("___DEBUG___")
+        person1Stat = list(person1Stat.values())[::-1]
+        person2Stat = list(person2Stat.values())[::-1]
+        print(dateStat)
+        exit()
+        values = {}
+        values["all"] = dateStatKValues
+
+        #exit()
+        values[person1] = person1Stat
+        values[person2] = person2Stat
+
         print(htmlFilename)
 
 
         data = {}
+        data["person1"] = person1
+        data["person2"] = person2
         data["words"] = words
-        data["person1"] = 1
         data["messages"] = myMessages
         data["diff"] =  getDate(timeDiff)
         data["first"] = getDate(firstMess)
@@ -321,7 +471,8 @@ def processJson(file):
         data["count"] = mess_count
         print(htmlFilename)
         print("ide " + data["last"])
-        content = generateContent(dateStatList, dateStatKValues, data)
+
+        content = generateContent(dateStatList, values, data)
 
         with open(htmlFilename, "w", encoding="utf-8") as f:
             f.write(content)
@@ -350,15 +501,23 @@ for file in onlyfiles:
     if (file.endswith('.json')):
         print("is json")
         processJson(file)
+        print("itt")
+        exit()
 counter = 0
+
 fileCounter = 0
 for file in onlyDirs:
     path =mypath2 + file + "\message_1.json"
+    path2 =mypath2 + file + "\message_2.json"
+    path3 =mypath2 + file + "\message_3.json"
+    path4 =mypath2 + file + "\message_4.json"
+    path5 =mypath2 + file + "\message_5.json"
+
 
     with open(path) as json_file:
         print(counter)
         counter += 1
-        print(json_file)
+        #print(json_file)
         data = json.load(json_file)
         for p in data['participants']:
              print('Name: ' + encodeText(p['name']))
@@ -373,7 +532,8 @@ for file in onlyDirs:
         print("Utolsó üzi : " + getDate(data['messages'][0]["timestamp_ms"]))
     #exit(0)
     print("\n")
-
     fileCounter += processJson(path)
-    if fileCounter == 1:
+    if fileCounter >= 2:
         exit()
+    if (os._exists(path2)):
+        fileCounter += processJson(path2)
